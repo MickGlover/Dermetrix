fa2110addcc20f441d63801f00b045032522243e
Michael Glover <mickglover1@gmail.com>
2015-09-23 11:26:47 -0400

    Created a basic layout for the app

--- app/src/main/java/com/example/mick/Dermetrix/MainActivity.java
+++ app/src/main/java/com/example/mick/Dermetrix/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.mick.Dermetrix;
 
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.content.Intent;
 import android.view.View;
+import android.util.Log;
 
 import com.example.mick.Dermetrix.R;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,38 +27,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "The onStart() event");
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "The onResume() event");
     }
 
-    public void sendMessage(View view) {
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"The onPause() event");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "the onStop() event");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"The onDestroy() event");
+    }
+
+    public void goToCalendarActivity(View view) {
+        Intent intent = new Intent(this, Calendar.class);
+        startActivity(intent);
+    }
+    public void goToFindTreatmentActivity(View view) {
+        Intent intent = new Intent(this, FindTreatment.class);
+        startActivity(intent);
+    }
+    public void goToPhotoRollActivity(View view) {
+        Intent intent = new Intent(this, PhotoRoll.class);
+        startActivity(intent);
+    }
+    public void goToRemindersActivity(View view) {
+        Intent intent = new Intent(this, Reminders.class);
+        startActivity(intent);
+    }
+    public void goToTakePictureActivity(View view) {
+        Intent intent = new Intent(this, TakePicture.class);
+        startActivity(intent);
+    }
+    public void goToTrackingActivity(View view) {
         Intent intent = new Intent(this, Tracking.class);
         startActivity(intent);
     }
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
+
 }
