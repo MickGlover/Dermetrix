cdcb67ca3c5ff8015d6ce8baaebcd0945a6f938c
Morganjack13 <morganjack13@yahoo.com>
2015-11-01 19:22:37 -0500

    new photo description activity
    also added a graph button

--- app/src/main/java/com/example/mick/Dermetrix/Tracking.java
+++ app/src/main/java/com/example/mick/Dermetrix/Tracking.java
@@ -1,9 +1,11 @@
 package com.example.mick.Dermetrix;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.mick.Dermetrix.R;
 
@@ -15,6 +17,9 @@ public class Tracking extends AppCompatActivity {
         setContentView(R.layout.activity_tracking);
     }
 
-
+    public void goToCalendarActivity(View view) {
+        Intent intent = new Intent(this, Calendar.class);
+        startActivity(intent);
+    }
 
 }
